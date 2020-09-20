c4d8b4995b4d234b2ec8d07a9c5103b1393c02d0
EC2 Default User <ec2-user@ip-172-31-90-147.ec2.internal>
2020-09-20 12:20:44 +0000

    Made scrollbar only visible when scrolling down

--- public/js/set-background.js
+++ public/js/set-background.js
@@ -53,3 +53,18 @@ body.className = "heaven-" + idx;
         }
 
         setInterval(draw, 35);
+
+document.getElementById("navbar").style.display = "none";
+
+//hide navbar
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+
+  // 20 is an arbitrary number here, just to make you think if you need the prevScrollpos variable:
+  if (currentScrollPos > 20) {
+    // I am using 'display' instead of 'top':
+    document.getElementById("navbar").style.display = "initial";
+  } else {
+    document.getElementById("navbar").style.display = "none";
+  }
+}
