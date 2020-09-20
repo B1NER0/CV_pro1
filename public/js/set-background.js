03c61e612bd0e46dbb82f55a1198a80839d36b01
EC2 Default User <ec2-user@ip-172-31-90-147.ec2.internal>
2020-09-20 17:30:09 +0000

    Fixed matrix resizing issue

--- public/js/set-background.js
+++ public/js/set-background.js
@@ -2,19 +2,17 @@ var idx = Math.floor((new Date().getHours()));
 var body = document.getElementsByTagName("body")[0];
 body.className = "heaven-" + idx;
 
+var tel = 0;
+var mWidth = window.innerWidth;
+var mHeight = window.innerHeight;
+
 // geting canvas by Boujjou Achraf
         var c = document.getElementById("c");
         var ctx = c.getContext("2d");
-
-        //making the canvas full screen
+        
         c.height = window.innerHeight;
         c.width = window.innerWidth;
-
-        //chinese characters - taken from the unicode charset
-        var matrix = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}";
-        //converting the string into an array of single characters
-        matrix = matrix.split("");
-
+        
         var font_size = 10;
         var columns = c.width/font_size; //number of columns for the rain
         //an array of drops - one per column
@@ -23,15 +21,37 @@ body.className = "heaven-" + idx;
         //1 = y co-ordinate of the drop(same for every drop initially)
         for(var x = 0; x < columns; x++)
             drops[x] = 1; 
+        
+        
+        //chinese characters - taken from the unicode charset
+            var matrix = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}";
+            //converting the string into an array of single characters
+            matrix = matrix.split("");
+        
+        window.onresize = function test(){
+            
+            ctx.clearRect(0,0, c.width, c.height);
+            
+            
+            c.height = window.innerHeight;
+            c.width = window.innerWidth;
+            
+            columns = c.width/font_size;
+            
+             for(var x = 0; x < columns; x++)
+            drops[x] = 1;
+        }
+        
 
+        //making the canvas full screen
+        
         //drawing the characters
         function draw()
         {
             //Black BG for the canvas
             //translucent BG to show trail
             ctx.fillStyle = "rgba(15, 20, 15, 0.04)";
-            ctx.fillRect(0, 0, c.width, c.height);
-
+            ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
             ctx.fillStyle = "#00ff00";//green text
             ctx.font = font_size + "px arial";
             //looping over drops
@@ -44,7 +64,7 @@ body.className = "heaven-" + idx;
 
                 //sending the drop back to the top randomly after it has crossed the screen
                 //adding a randomness to the reset to make the drops scattered on the Y axis
-                if(drops[i]*font_size > c.height && Math.random() > 0.975)
+                if(drops[i]*font_size > window.innerHeight && Math.random() > 0.975)
                     drops[i] = 0;
 
                 //incrementing Y coordinate
